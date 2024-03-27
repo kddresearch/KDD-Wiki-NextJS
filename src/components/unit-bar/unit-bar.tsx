0788da79cd41760282c49faf8719d8e1c62a866a
Legonois <74224353+Legonois@users.noreply.github.com>
2024-03-27 00:26:49 -0500

    Fixed wildcat image and added login/logout href

--- src/components/unit-bar/unit-bar.tsx
+++ src/components/unit-bar/unit-bar.tsx
@@ -8,10 +8,14 @@ export default function UnitBar({ title, kdduser }: { title: string, kdduser: an
 
   const user = kdduser;
 
+  const onLogin = () => {
+    // redirect to login page
+  }
+
   return (
-    <div className="flex items-center justify-between w-full h-16 bg-white text-purple">
+    <div className="flex items-center justify-between w-full h-16 lg:h-14 bg-white text-purple">
       <div className="container flex items-center">
-        <Image src="/wildcat.svg" alt="unit" width="75" height="75" className="pr-4 h-8" />
+        <Image src="/wildcat.svg" alt="unit" width="40" height="32" className="mr-4 h-8" />
         <div id="bar" className="realitive pr-8 border-l-[1px] border-purple border-solid h-8 w-1"></div>
         <Link className="grow" href={"/"}>
           <p className="text-2xl font-bold overflow-hidden line-clamp-2" title={title}>
@@ -19,17 +23,17 @@ export default function UnitBar({ title, kdduser }: { title: string, kdduser: an
           </p>
         </Link>
         {/* Signin */}
-        <div className="flex items-center justify-end">
+        <div className="flex items-center justify-end ml-2">
           {user.username !== "Guest" ? (
             // sign out
-            <button className="h-8 px-4 border-purple border-solid border-2 rounded-full min-w-max">
+            <Link href={"/signout"} className="h-8 px-4 border-purple border-solid border-2 rounded-full min-w-max">
               Sign out
-            </button>
+            </Link>
             // notification
           ) : (
-            <button className="h-8 px-4 border-purple border-solid border-2 rounded-full min-w-max">
+            <Link href={"/signin"} className="h-8 px-4 border-purple border-solid border-2 rounded-full min-w-max">
               Sign in
-            </button>
+            </Link>
           )}
         </div>
       </div>
