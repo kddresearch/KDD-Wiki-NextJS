73382df51631e9b56eb06e17e60fec453e2ff2da
Legonois <74224353+Legonois@users.noreply.github.com>
2024-03-28 00:35:02 -0500

    Secured the route

--- src/app/api/pages/route.ts
+++ src/app/api/pages/route.ts
@@ -1,9 +1,21 @@
 "use server";
 
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { Page, fetchAll } from "@/app/lib/models/page";
 
-export const GET = async () => {
+import { auth } from "@/auth";
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+
+  // const session = await auth(req, res);
+  const session = await auth();
+
+  console.log('session:', session?.user);
+
+  if (session?.user == undefined) {
+    return NextResponse.json({ error: 'Unauthorized', status: 401 })
+  }
 
   try {
     const pages = await fetchAll();
