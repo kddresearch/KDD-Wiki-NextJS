ebfb3252a98296695552b8a35cb75cba72c4391b
Legonois <wesley.baldwin@outlook.com>
2024-04-15 10:18:46 -0500

    fixed tests

--- __tests__/vitest.test.tsx
+++ __tests__/vitest.test.tsx
@@ -3,3 +3,7 @@ import { render, screen } from '@testing-library/react'
 // import WikiUser from './app/lib/models/wikiuser'
 import WikiUser, { AccessLevel } from "@/app/lib/models/wikiuser"
 
+test('test' , () => {
+    const test = true;
+    expect(test).toBe(true);
+});
