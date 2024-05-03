4a16bedd6547537618fff8f9c98ab4c6914d1571
Legonois <74224353+Legonois@users.noreply.github.com>
2024-05-03 16:47:35 -0500

    Rewrote some Markdown Parsing for MDX

--- src/app/lib/utils/markdown.tsx
+++ src/app/lib/utils/markdown.tsx
@@ -1,35 +1,40 @@
 import fs from 'fs';
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import Markdown from 'react-markdown'
-import classNames from "classnames";
+import React from 'react';
+import classNames from 'classnames';
+import rehypeParse from 'rehype-parse';
+import rehypeReact from 'rehype-react';
+import {unified} from 'unified';
 
-function RenderMarkdownString({
+// @ts-expect-error: the react types are missing.
+const production = {Fragment: prod.Fragment, jsx: prod.jsx, jsxs: prod.jsxs}
+
+function RenderMarkdownString({ 
   markdown, 
   ...props 
-} : { markdown: string } & React.HTMLAttributes<HTMLDivElement>) {
+}: { 
+  markdown: string } & 
+  React.HTMLAttributes<HTMLDivElement>
+) {
+  const proseClasses = classNames('prose', 'max-w-none', 'prose-h1:text-purple', 'prose-a:text-purple', 'prose-a:underline', props.className);
 
-  const proseClasses = classNames(
-    'prose',
-    'max-w-none',
-    'prose-h1:text-purple',
-    'prose-a:text-purple',
-    'prose-a:underline',
-    props.className
-  );
+  const processor = unified()
+    .use(rehypeParse, {fragment: true})
+    .use(rehypeReact, production)
 
-  return (<Markdown className={proseClasses}>{markdown}</Markdown>);
+  const content = processor.processSync(markdown).result;
+
+  return <div className={proseClasses} {...props}>{content}</div>;
 }
 
-function RenderMarkdownFile({
+function RenderMarkdownFile({ 
   filePath, 
   ...props 
-} : { filePath: string } & React.HTMLAttributes<HTMLDivElement>) {
+}: { 
+  filePath: string } & 
+  React.HTMLAttributes<HTMLDivElement>
+) {
   const fileContent = fs.readFileSync(filePath, 'utf8');
-  return (<RenderMarkdownString markdown={fileContent} {...props}/>);
+  return <RenderMarkdownString markdown={fileContent} {...props} />;
 }
 
-export {
-  RenderMarkdownFile,
-  RenderMarkdownString
-}
+export { RenderMarkdownFile, RenderMarkdownString };
