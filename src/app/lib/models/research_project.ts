45681935acd5f3d138098eb197dee9ec3900d8f5
Legonois <74224353+Legonois@users.noreply.github.com>
2024-05-03 16:47:35 -0500

    Rewrote some Markdown Parsing for MDX

--- src/app/lib/models/research_project.ts
+++ src/app/lib/models/research_project.ts
@@ -1,11 +1,105 @@
-import Joi from "joi";
+// Example Project Data
+const researchProject = {
+    id: 1,
+    title: "Topical Knowledge Mapping",
+    researchCategory: {
+        name: "Natural Language Processing",
+        leadResearher: "Dr. William Hsu",
+    },
+    description: "This project aims to create a knowledge graph of research topics in the field of Natural Language Processing.",
+    tags: ["Natural Language Processing", "Knowledge Graph", "Topic Modeling"], // also works as keywords
+    methodology: "We will use topic modeling techniques to extract topics from research papers in the ACL Anthology.",
+    personnel: [
+        {
+            name: "Dr. William Hsu",
+            role: "Principal Investigator",
+            email: "johnd@k-state.edu",
+            affiliation: "Kansas State University"
+        },
+        {
+            name: "Jane Smith",
+            role: "Research Assistant",
+            email: "JaneS@k-state.edu",
+            affiliation: "Kansas State University"
+        },
+        {
+            name: "Jane Doe",
+            role: "Affiliated Researcher",
+            email: "JaneS@cmu.edu",
+            affiliation: "Carnegie Mellon University"
+        },
+        {
+            name: "John Smith",
+            role: "Alumni",
+            email: "JaneS@k-state.edu",
+            affiliation: "Kansas State University"
+        },
+    ],
+    publications: [
+        {
+            title: "Topical Knowledge Mapping: A Survey",
+            authors: ["John Doe", "Jane Smith"],
+            publicationDate: "2021-09-01",
+            publicationVenue: "ACL"
+        },
+    ],
+    datasets: [
+        {
+            name: "ACL Anthology",
+            description: "A collection of research papers in the field of Natural Language Processing.",
+            source: "https://aclanthology.org/dataset/52",
+            isPrivate: false,
+        },
+        {
+            name: "Unpublished Dataset",
+            description: "A dataset of research papers that have not been published yet",
+            source: "https://unpublished.aclanthology.org/dataset/23",
+            isPrivate: true,
+        },
+    ],
+    sourceCode: {
+        url: "github.com/kddresearch/topical-knowledge-mapping",
+        isPrivate: false,
+    },
+    funding: {
+        source: "National Science Foundation",
+        awardNumber: "NSF-123456",
+        awardAmount: 50_000,
+        dateAwarded: "2021-01-01",
+    }
+};
+
+import Joi, { State } from "joi";
+import remark from 'remark';
+import remarkParse from 'remark-parse';
+import { unified } from 'unified';
+import { CustomHelpers } from 'joi';
+import remarkMdx from 'remark-mdx'
+
+const mdxValidator = async (value: string, helpers: CustomHelpers) => {
+    try {
+      const processor = unified().use(remarkParse);
+      await processor.run(processor.parse(value));
+      return value;
+    } catch (error) {
+      const errorState: State = {
+        key: '',
+        path: [],
+        parent: null,
+        reference: null,
+        ancestors: [],
+      };
+  
+      return helpers.error('string.mdx', { value }, errorState);
+    }
+  };
 
 // Joi schema for the ResearchProject validation
 const researchProjectSchema = Joi.object({
     id: Joi.number().required(),
-    title: Joi.string().required(),
+    title: Joi.string().alphanum().max(50).min(4).required(),
     category_id: Joi.number().required(),
-    description: Joi.string().required(),
+    description: Joi.string().max(2000).required(),
     project_management_link: Joi.string().required(),
     datasets_id: Joi.array().items().required(),
     source_code: Joi.object({
@@ -17,40 +111,4 @@ const researchProjectSchema = Joi.object({
     date_modified: Joi.date().required(),
 });
 
-class ResearchProject {
-    id: number;
-    title: string;
-    description: string;
-    project_management_link: string;
-    source_code: { url: string, is_private: boolean };
-
-    // relationships
-    category_id: number;
-    datasets_id: number[];
-
-    date_created: Date;
-    date_modified: Date;
-
-    constructor(data: any) {
-        // Validate the data
-        const { error, value } = researchProjectSchema.validate(data);
-
-        // If there is an error, throw an error
-        if (error) {
-            throw new Error(`ResearchProject validation error: ${error.message}`);
-        }
-
-        this.id = value.id;
-        this.title = value.title;
-        this.category_id = value.category_id;
-        this.description = value.description;
-        this.project_management_link = value.project_management_link;
-        this.datasets_id = value.datasets_id;
-        this.source_code = value.source_code;
-
-        this.date_created = value.date_created;
-        this.date_modified = value.date_modified;
-    }
-}
-
-export default ResearchProject;
+export default researchProject;
