0ffdc171c7dce5c73c2b015ac48544ee92ac9deb
Legonois <74224353+Legonois@users.noreply.github.com>
2024-05-03 17:16:24 -0500

    Remote MDX now?

--- src/app/lib/utils/markdown.tsx
+++ src/app/lib/utils/markdown.tsx
@@ -1,16 +1,11 @@
 import fs from 'fs';
-// import * as prod from 'react/jsx-runtime'
 import React from 'react';
 import classNames from 'classnames';
-// import rehypeParse from 'rehype-parse';
-// import rehypeSanitize from 'rehype-sanitize'
-// import rehypeStringify from 'rehype-stringify'
-// import rehypeReact from 'rehype-react';
-// import { unified } from 'unified';
 import { MDXRemote } from 'next-mdx-remote/rsc'
+import Card from '@/components/layout/card';
+import Link from 'next/link';
 
-// TODO: Fix this type issue
-// const production = {Fragment: prod.Fragment, jsx: prod.jsx, jsxs: prod.jsxs} as any;
+const components = { Card, Link };
 
 async function RenderMarkdownString({ 
   markdown, 
@@ -21,19 +16,9 @@ async function RenderMarkdownString({
 ) {
   const proseClasses = classNames('prose', 'max-w-none', 'prose-h1:text-purple', 'prose-a:text-purple', 'prose-a:underline', props.className);
 
-  // const processor = unified()
-  //   .use(rehypeParse, {fragment: true})
-  //   .use(rehypeSanitize)
-  //   .use(rehypeStringify)
-  //   .use(rehypeReact, production)
-
-  // const content = await processor.process(markdown);
-
-  // const content = <MDXRemote source={markdown} />
-
   return (
     <div className={proseClasses} {...props}>
-      <MDXRemote source={markdown} />
+      <MDXRemote source={markdown} components={components} />
     </div>
   );
 }
