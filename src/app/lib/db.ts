c21e5ccabc5e5dc547f3a227606c73a69b2663a0
Legonois <74224353+Legonois@users.noreply.github.com>
2024-05-07 16:44:55 -0500

    Migrated to new DB!

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,14 +1,18 @@
 import { Pool, QueryResult } from "pg";
 import env_config from "@/config";
 
-const pool = new Pool({
+const poolconfig = {
     user: env_config.db.username,
     host: env_config.db.host,
     database: env_config.db.name,
     password: env_config.db.password,
     port: env_config.db.port,
     ssl: true,
-});
+}
+
+// console.log("poolconfig: ", poolconfig);
+
+const pool = new Pool(poolconfig);
 
 var connected = false;
 
@@ -29,42 +33,42 @@ function connect(): void {
  * Executes a query on the PostgreSQL database | ***use specific query functions instead***
  */
 async function query(text: string, params?: any[]): Promise<QueryResult> {
-  if (!connected) {
-    connect();
-  }
-
-  // time the query
-  // const start = Date.now();
-
-  try {
-    // console Debug
-    // console.log('Executing query:', text + "\n" + params);
-    const result = await pool.query(text, params);
+    if (!connected) {
+        connect();
+    }
 
     // time the query
-    // const duration = Date.now() - start;
-    // console.log("table : ", text);
-    // console.log("Query duration: ", duration);
+    // const start = Date.now();
 
-    return result;
-  } catch (err) {
-    console.error("Error occurred during query execution:", err);
-    console.error("Query: ", text + "\n" + params);
+    try {
+        // console Debug
+        // console.log('Executing query:', text + "\n" + params);
+        const result = await pool.query(text, params);
 
-    throw err;
-  }
+        // time the query
+        // const duration = Date.now() - start;
+        // console.log("table : ", text);
+        // console.log("Query duration: ", duration);
+
+        return result;
+    } catch (err) {
+        console.error("Error occurred during query execution:", err);
+        console.error("Query: ", text + "\n" + params);
+
+        throw err;
+    }
 }
 
 // can connect
 async function testConnection(): Promise<boolean> {
-  try {
-    connect();
-    await query("SELECT NOW()");
-    return true;
-  } catch (err) {
-    console.error("Error occurred during testConnection:", err);
-    return false;
-  }
+    try {
+        connect();
+        await query("SELECT NOW()");
+        return true;
+    } catch (err) {
+        console.error("Error occurred during testConnection:", err);
+        return false;
+    }
 }
 
 export { query, testConnection };
